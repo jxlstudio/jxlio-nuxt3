9bdd7bba1c2a28b38a848f5be6cce21ac790f905
JXL Studio <jeremy@jxl.io>
2023-03-14 13:51:03 -0400

    gql test2

--- app.vue
+++ app.vue
@@ -7,12 +7,4 @@
 html {
   @apply scroll-smooth bg-black text-white;
 }
-.fade-enter-active,
-.fade-leave-active {
-  transition: all 1s;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
